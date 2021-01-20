920bf0d1cb7287f272146f3ae3c74ea417f435ba
Ria Talwar <riatalwar@hotmail.com>
2021-01-19 18:19:18 -0800

    Add test functions

--- Unit 2/TriangleSidesPossibility.py
+++ Unit 2/TriangleSidesPossibility.py
@@ -8,15 +8,30 @@ def is_possible(sides):
     return longest < sumOfShorter
 
 def determine_change(sides):
-    isPossible = is_possible(sides)
-    if isPossible:
+    if is_possible(sides):
         return 'This triangle is possible.'
     else:
         longest = max(sides)
         sumOfShorter = sum(sides.remove(longest))
         changeNecessary = longest - sumOfShorter
-        return 'The shortest side must be increased in length by at least ' + changeNecessary + 'units.'
+        return 'The shortest side must be increased in length by more than ' + str(changeNecessary) + ' units.'
 
 def run():
     sides = [int(input('Enter a side length: ')) for i in range(3)]
     print(determine_change(sides))
+
+def test_is_possible():
+    print(is_possible([1, 3, 3]) == True)
+    print(is_possible([5, 4, 7]) == True)
+    print(is_possible([1, 2, 3]) == False)
+    print(is_possible([0, 3, 3]) == False)
+    print(is_possible([1, 4, 9]) == False)
+
+def test_determine_change():
+    print(determine_change([1, 3, 3]) == 'This triangle is possible.')
+    print(determine_change([0, 3, 3]) == 'The shortest side must be increased in length by more than 0 units.')
+    print(determine_change([1, 4, 9]) == 'The shortest side must be increased in length by more than 4 units.')
+
+def test():
+    test_is_possible()
+    test_determine_change()
