dbcac5513baf8f09715eb562820353aae6957012
BillieEyelash <riatalwar@hotmail.com>
2021-01-19 19:29:27 -0800

    Add function run()

--- Unit 2/MarinersChallenge.py
+++ Unit 2/MarinersChallenge.py
@@ -1,4 +1,4 @@
-def calculate_wins_losses(homeTeamRuns, opponentRuns)
+def calculate_wins_losses(homeTeamRuns, opponentRuns):
     winsLosses = {'wins': 0, 'losses': 0, 'ties': 0}
     for run in range(len(homeTeamRuns)):
         if homeTeamRuns[run] > opponentRuns[run]:
@@ -8,3 +8,13 @@ def calculate_wins_losses(homeTeamRuns, opponentRuns)
         else:
             winsLosses['ties'] += 1
     return winsLosses
+
+def run():
+    marinerRuns = [10, 7, 8, 2, 3, 3, 5, 0, 6, 1]
+    opponentRuns = [3, 5, 2, 3, 1, 6, 3, 2, 4, 3]
+    winsLosses = calculate_wins_losses(marinerRuns, opponentRuns)
+    print("The mariners had...")
+    for key, value in winsLosses.items():
+        print(value, key)
+
+run()
