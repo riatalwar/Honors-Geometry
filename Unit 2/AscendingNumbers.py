b072f9a88748008048f8f087f82eb26e947fd174
Ria Talwar <riatalwar@hotmail.com>
2021-01-19 16:30:54 -0800

    Complete sort() function

--- Unit 2/AscendingNumbers.py
+++ Unit 2/AscendingNumbers.py
@@ -2,14 +2,20 @@
 # Ascending Numbers
 # Sort 3 numbers into ascending order
 
-numbers = [int(input('Enter an integer: ')) for i in range(3)]
-if numbers[0] < numbers[1] and numbers[0] < numbers[2]:
-    if numbers[1] < numbers[2]:
-        sorted = [numbers[0], numbers[1], numbers[2]]
+
+def sort(numbers):
+    if numbers[0] < numbers[1] and numbers[0] < numbers[2]:
+        if numbers[1] < numbers[2]:
+            return [numbers[0], numbers[1], numbers[2]]
+        else:
+            return [numbers[0], numbers[2], numbers[1]]
+    elif numbers[1] < numbers[0] and numbers[1] < numbers[2]:
+        if numbers[0] < numbers[2]:
+            return [numbers[1], numbers[0], numbers[2]]
+        else:
+            return [numbers[1], numbers[2], numbers[1]]
     else:
-        sorted = [numbers[0], numbers[2], numbers[1]]
-elif numbers[1] < numbers[0] and numbers[1] < numbers[2]:
-    if numbers[0] < numbers[2]:
-        sorted = [numbers[1], numbers[0], numbers[2]]
-    else:
-        sorted = [numbers[1], numbers[2], numbers[1]]
+        if numbers[0] < numbers[1]:
+            return [numbers[2], numbers[0], numbers[1]]
+        else:
+            return [numbers[2], numbers[1], numbers[0]]
