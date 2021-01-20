94ec7d1903efd95f5b1cff3536d810115ec8bd6d
Ria Talwar <riatalwar@hotmail.com>
2021-01-19 17:14:34 -0800

    Add instructions

---
+++ Unit 2/TriangleSidesPossibility.py
@@ -0,0 +1,3 @@
+# Have the user enter in 3 lengths for the sides of a triangle. Check to see if the triangle is possible.
+# If it is, tell them a triangle is possible.
+# If not, tell them how much longer the shortest leg would need to be to make it possible.
