dcbaed7cf8528816ba5c94ff2ae7436bfdceeae5
Ria Talwar <riatalwar@hotmail.com>
2021-01-19 16:51:52 -0800

    Add evaluate() function

--- Unit 2/EvaluateFeeling.py
+++ Unit 2/EvaluateFeeling.py
@@ -2,11 +2,13 @@
 # Evaluate Feeling
 # Practice with Boolean variables and logical operators
 
-tired = input('Are you feeling tired today? ').lower()[0]
-if tired == 'y':
-    print('Maybe you should try getting some more sleep.')
-elif tired != 'n':
-    print("That's not a valid input!")
-else:
-    print('You have some excellent sleep habits!')
-print('Have a wonderful day!')
+def evaluate(isTired):
+    if isTired == 'y':
+        return 'Maybe you should try getting some more sleep.'
+    elif isTired == 'n':
+        return 'You have some excellent sleep habits!'
+    else:
+        return "That's not a valid input!"
+
+isTired = input('Are you feeling tired today? ').lower()[0]
+print(evaluate(isTired), '\nHave a wonderful day!')
