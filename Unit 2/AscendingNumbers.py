49d4a5ff6ec8f57be81a4acbba9cafe33c0a40bf
Ria Talwar <riatalwar@hotmail.com>
2021-01-19 16:38:37 -0800

    Bug fix (replace < with <=) and test function

--- Unit 2/AscendingNumbers.py
+++ Unit 2/AscendingNumbers.py
@@ -4,18 +4,18 @@
 
 
 def sort(numbers):
-    if numbers[0] < numbers[1] and numbers[0] < numbers[2]:
-        if numbers[1] < numbers[2]:
+    if numbers[0] <= numbers[1] and numbers[0] <= numbers[2]:
+        if numbers[1] <= numbers[2]:
             return [numbers[0], numbers[1], numbers[2]]
         else:
             return [numbers[0], numbers[2], numbers[1]]
-    elif numbers[1] < numbers[0] and numbers[1] < numbers[2]:
-        if numbers[0] < numbers[2]:
+    elif numbers[1] <= numbers[0] and numbers[1] <= numbers[2]:
+        if numbers[0] <= numbers[2]:
             return [numbers[1], numbers[0], numbers[2]]
         else:
             return [numbers[1], numbers[2], numbers[1]]
     else:
-        if numbers[0] < numbers[1]:
+        if numbers[0] <= numbers[1]:
             return [numbers[2], numbers[0], numbers[1]]
         else:
             return [numbers[2], numbers[1], numbers[0]]
@@ -24,3 +24,13 @@ def run():
     numbers = [int(input('Enter an integer: ')) for i in range(3)]
     sorted = sort(numbers)
     print(sorted[0], sorted[1], sorted[2])
+
+def test():
+    print(sort([1, 2, 3]) == [1, 2, 3])
+    print(sort([3, 2, 1]) == [1, 2, 3])
+    print(sort([1, 3, 2]) == [1, 2, 3])
+    print(sort([1, 1, 3]) == [1, 1, 3])
+    print(sort([4, 4, 3]) == [3, 4, 4])
+    print(sort([5, 8, 5]) == [5, 5, 8])
+
+test()
