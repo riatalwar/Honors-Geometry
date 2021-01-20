57937d3f16c1013b9837f581d74197ae7e8f7d2c
Ria Talwar <riatalwar@hotmail.com>
2021-01-19 17:48:41 -0800

    Add function is_possible()

--- Unit 2/TriangleSidesPossibility.py
+++ Unit 2/TriangleSidesPossibility.py
@@ -1,3 +1,8 @@
 # Have the user enter in 3 lengths for the sides of a triangle. Check to see if the triangle is possible.
 # If it is, tell them a triangle is possible.
 # If not, tell them how much longer the shortest leg would need to be to make it possible.
+
+def is_possible(sides):
+    longest = max(sides)
+    sumOfShorter = sum(sides.remove(longest))
+    return longest < sumOfShorter
