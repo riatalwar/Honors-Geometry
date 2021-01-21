66f8f2085c06777802db37450fda671c3bb1765b
BillieEyelash <riatalwar@hotmail.com>
2021-01-21 12:26:14 -0800

    Add function test()

--- Unit 2/MyFirstFunction.py
+++ Unit 2/MyFirstFunction.py
@@ -9,4 +9,13 @@ def distance(pX, pY, qX, qY):
     return: distance between two points (float)
     """
     dist = ((qY - pY)**2 + (qX - pX)**2)**(1/2)
-    return dist
+    return round(dist, 2)
+
+def test():
+    '''test() -> test cases for distance()'''
+    print(distance(0, 0, 3, 4) == 5)
+    print(distance(0, 0, 0, 0) == 0)
+    print(distance(2, 2, 2, 2) == 0)
+    print(distance(4, 9, -8, -2) == 16.28)
+
+test()
