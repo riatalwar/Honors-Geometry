9514815297f76d8b5bf59771e16855b133eb3761
BillieEyelash <riatalwar@hotmail.com>
2021-01-21 08:53:34 -0800

    Add comments

--- Unit 2/MarinersChallenge.py
+++ Unit 2/MarinersChallenge.py
@@ -1,5 +1,11 @@
+# Ria T., Honors Geometry, 1/20/21
+# Mariners Challenge Problem
+# Count the number of wins vs the number of losses based on Mariners data
+
 def calculate_wins_losses(homeTeamRuns, opponentRuns):
-    winsLosses = {'wins': 0, 'losses': 0, 'ties': 0}
+    '''calculate_wins_losses() -> dict
+    containing the number of wins/losses/ties'''
+    winsLosses = {'wins': 0, 'losses': 0, 'ties': 0} # init dict to track wins, losses, and ties
     for run in range(len(homeTeamRuns)):
         if homeTeamRuns[run] > opponentRuns[run]:
             winsLosses['wins'] += 1
@@ -10,6 +16,8 @@ def calculate_wins_losses(homeTeamRuns, opponentRuns):
     return winsLosses
 
 def run():
+    ''''run() -> analyze mariners
+    data and print the results'''
     marinerRuns = [10, 7, 8, 2, 3, 3, 5, 0, 6, 1]
     opponentRuns = [3, 5, 2, 3, 1, 6, 3, 2, 4, 3]
     winsLosses = calculate_wins_losses(marinerRuns, opponentRuns)
