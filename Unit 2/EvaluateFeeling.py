365df626155ad018ddf8fa7f0b180fd481f1f1ad
BillieEyelash <riatalwar@hotmail.com>
2021-01-21 08:49:46 -0800

    Add comments

--- Unit 2/EvaluateFeeling.py
+++ Unit 2/EvaluateFeeling.py
@@ -1,21 +1,25 @@
-# Ria T., Honors Geometry, 9/26/20
+# Ria T., Honors Geometry, 1/20/21
 # Evaluate Feeling
 # Practice with Boolean variables and logical operators
 
 def evaluate(isTired):
-    isTired = isTired.lower()[0]
+    '''evaluate(isTired) -> evaluate isTired
+    and return appropriate message'''
+    isTired = isTired.lower()[0]    # take only a lowercase version of the first letter to evaluate
     if isTired == 'y':
         return 'Maybe you should try getting some more sleep.'
     elif isTired == 'n':
         return 'You have some excellent sleep habits!'
-    else:
+    else:                           # if answer is neither yes nor no
         return "That's not a valid input!"
 
 def run():
+    '''run() -> get user input and print messages'''
     isTired = input('Are you feeling tired today? ')
     print(evaluate(isTired), '\nHave a wonderful day!')
 
 def test():
+    '''test() -> test cases for evaluate()'''
     print(evaluate('Y') == 'Maybe you should try getting some more sleep.')
     print(evaluate('yes') == 'Maybe you should try getting some more sleep.')
     print(evaluate('No') == 'You have some excellent sleep habits!')
