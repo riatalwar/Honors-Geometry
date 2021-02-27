0a7fedcede3065e2640444d68005c4cd08a84926
BillieEyelash <riatalwar@hotmail.com>
2021-02-27 09:13:24 -0800

    Create TriangleAngle.py and functions is_possible() and find_angle()

---
+++ Unit-2/TriangleAngle.py
@@ -0,0 +1,25 @@
+# Ria T., Honors Geometry, 2/27/21
+# Determine Triangle Angle
+# Use user input for the sides of a triangle and determine whether the angle is obtuse, right, or acute
+
+def is_possible(sides):
+    '''is_possible() -> whether a
+    triangle is possible'''
+    sides.sort()
+    longest = sides[2]
+    sumOfShorter = sum(sides) - longest
+    return longest < sumOfShorter
+
+def find_angle(sides):
+    '''find_angle() -> whether an
+    angle is obtuse, right, or acute'''
+    sides.sort()
+    longestSquared = sides[2] ** 2
+    legsSquared = sides[0] ** 2 + sides[1] ** 2
+    
+    if longestSquared >= legsSquared:
+        return "obtuse"
+    elif longestSquared == legsSquared:
+        return "right"
+    else:
+        return "acute"
