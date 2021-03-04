5ba865da2396e15e1d509fe1f8de3f6a0d721a3e
BillieEyelash <riatalwar@hotmail.com>
2021-03-04 13:39:13 -0800

    Create file TriangleMeasures.py

---
+++ Unit-2/TriangleMeasures.py
@@ -0,0 +1,24 @@
+"""
+Program Summary:
+This program will take a list of triangle measures for triangle ABC - side lenghts (a,b,c)
+followed by angle measures (A,B,C) - and display these in a user-friendly format.
+"""
+
+##INPUT
+
+# Leave this blank...(nothing to do here)
+
+
+##PROCESSING
+
+SideAngleMeasures = [3.6, 2.0, 5.1, 33.30240516500015, 17.76009924291366, 128.93749559208618]
+
+
+##OUTPUT
+
+Results for your triangle ABC:
+a = 3.6, b = 2.0, c = 5.1
+A = 33.3, B = 17.76, C = 128.94
+
+(1) all output values must be rounded to 2 decimal places; and
+(2) the output needs to match exactly (every character matters)
