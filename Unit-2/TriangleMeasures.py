eb2d59267d92e3a6b23b7d27e15f042fc4e88d69
BillieEyelash <riatalwar@hotmail.com>
2021-03-06 10:19:01 -0800

    Final submission

--- Unit-2/TriangleMeasures.py
+++ Unit-2/TriangleMeasures.py
@@ -4,21 +4,14 @@ This program will take a list of triangle measures for triangle ABC - side lengh
 followed by angle measures (A,B,C) - and display these in a user-friendly format.
 """
 
-##INPUT
+# NO INPUT
 
-# Leave this blank...(nothing to do here)
-
-
-##PROCESSING
-
+# PROCESSING
 SideAngleMeasures = [3.6, 2.0, 5.1, 33.30240516500015, 17.76009924291366, 128.93749559208618]
+for i in range(len(SideAngleMeasures)):
+    SideAngleMeasures[i] = round(SideAngleMeasures[i], 2)
 
-
-##OUTPUT
-
-Results for your triangle ABC:
-a = 3.6, b = 2.0, c = 5.1
-A = 33.3, B = 17.76, C = 128.94
-
-(1) all output values must be rounded to 2 decimal places; and
-(2) the output needs to match exactly (every character matters)
+# OUTPUT
+print("Results for your triangle ABC:")
+print("a = " + str(SideAngleMeasures[0]) + ", b = " + str(SideAngleMeasures[1]) + ", c = " + str(SideAngleMeasures[2]))
+print("A = " + str(SideAngleMeasures[3]) + ", B = " + str(SideAngleMeasures[4]) + ", C = " + str(SideAngleMeasures[5]))
